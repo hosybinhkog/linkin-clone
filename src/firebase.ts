701b5618ed51f4e509175e17ffbe4c2e603a27ba
hosybinhkog <hosybinhkog@gmail.com>
2022-09-14 20:46:28 +0700

    start

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDd0Z3YGEmtEiLXKPhf8xh-lqQ_0thhjmA",
+  authDomain: "linkin-clone-82f49.firebaseapp.com",
+  projectId: "linkin-clone-82f49",
+  storageBucket: "linkin-clone-82f49.appspot.com",
+  messagingSenderId: "263971820377",
+  appId: "1:263971820377:web:3616b6c4d679547c0d3248",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
